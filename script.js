37250eb803e4162dc6c8475a493fd3a220d952b9
Sam Farrell <merriman2948@gmail.com>
2022-06-15 11:50:33 -0400

    Created seperate container for eachrow. Added flex to all containers in CSS. Duplicated Div Creationg Function in script.

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const container = document.querySelector('.containerOne');
+const containerOne = document.querySelector('.containerOne');
+const containerTwo = document.querySelector('.containerTwo');
+const containerThree = document.querySelector('.containerThree');
+const containerFour = document.querySelector('.containerFour');
+const containerFive = document.querySelector('.containerFive');
+const containerSix = document.querySelector('.containerSix');
+const containerSeven = document.querySelector('.containerSeven');
+const containerEight = document.querySelector('.containerEight');
+const containerNine = document.querySelector('.containerNine');
+const containerTen = document.querySelector('.containerTen');
+const containerEleven = document.querySelector('.containerEleven');
+const containerTwelve = document.querySelector('.containerTwelve');
+const containerThirteen = document.querySelector('.containerThirteen');
+const containerFourteen = document.querySelector('.containerFourteen');
+const containerFifteen = document.querySelector('.containerFifteen');
+const containerSixteen = document.querySelector('.containerSixteen');
 
-function createDiv(){
+function createDivOne(){
 
     var div = document.createElement('div');
     div.style.width = '10px';
@@ -11,11 +26,264 @@ function createDiv(){
     div.style.border = '10px';
     div.style.padding = '10px';
 
-    container.appendChild(div);
+    containerOne.appendChild(div);
 };
 
+for(let i = 0; i < 16; i++) {
+    createDivOne();
+};
 
+function createDivTwo(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerTwo.appendChild(div);
+};
 
 for(let i = 0; i < 16; i++) {
-    createDiv();
-}
+    createDivTwo();
+};
+
+function createDivThree(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerThree.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivThree();
+};
+
+function createDivFour(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerFour.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivFour();
+};
+
+function createDivFive(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerFive.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivFive();
+};
+
+function createDivSix(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerSix.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivSix();
+};
+
+function createDivSeven(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerSeven.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivSeven();
+};
+
+function createDivEight(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerEight.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivEight();
+};
+
+function createDivNine(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerNine.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivNine();
+};
+
+function createDivTen(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerTen.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivTen();
+};
+
+function createDivEleven(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerEleven.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivEleven();
+};
+
+function createDivTwelve(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerTwelve.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivTwelve();
+};
+
+function createDivThirteen(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerThirteen.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivThirteen();
+};
+
+function createDivFourteen(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerFourteen.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivFourteen();
+};
+
+function createDivFifteen(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerFifteen.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivFifteen();
+};
+
+function createDivSixteen(){
+
+    var div = document.createElement('div');
+    div.style.width = '10px';
+    div.style.height = '10px';
+    div.style.background = 'red';
+    div.style.margin = '10px';
+    div.style.border = '10px';
+    div.style.padding = '10px';
+
+    containerSixteen.appendChild(div);
+};
+
+for(let i = 0; i < 16; i++) {
+    createDivSixteen();
+};
